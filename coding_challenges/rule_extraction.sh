a0195002097cfed5945e1198647811dd3834edba
vivekvardhanadepu <vivekvicky839@gmail.com>
2021-04-26 15:15:02 +0530

    "not in index" error resolved

--- coding_challenges/rule_extraction.sh
+++ coding_challenges/rule_extraction.sh
@@ -11,16 +11,21 @@ python3 $SCRIPTS/ngram-count-patterns-maxent2.py data-$SL-$TL/$CORPUS.lex.$SL-$T
 
 echo -n "" > data-$SL-$TL/all-lambdas
 cat data-$SL-$TL/events | grep -v -e '\$ 0\.0 #' -e '\$ 0 #' > data-$SL-$TL/events.trimmed
-for i in `cat data-$SL-$TL/events.trimmed | cut -f1 | sort -u | sed 's/\([\*\^\$]\)/\\\\\1/g'`; do
-
+cat data-$SL-$TL/events.trimmed | cut -f1 | sort -u | sed 's/\([\*\^\$]\)/\\\\\1/g' > tmp.sl
+# OLDIFS=$IFS; IFS='\n';
+# for i in $(cat tmp.sl); do
+while read i; do
+	# IFS=$OLDIFS
 	num=`cat data-$SL-$TL/events.trimmed | grep "^$i" | cut -f2 | head -1`
-	echo $num > tmp.yasmet.$i;
-	cat data-$SL-$TL/events.trimmed | grep "^$i" | cut -f3  >> tmp.yasmet.$i;
+	echo $num > 'tmp.yasmet.$i';
+	cat data-$SL-$TL/events.trimmed | grep "^$i" | cut -f3  >> 'tmp.yasmet.$i';
 	echo "$i"
-	cat tmp.yasmet.$i | $YASMET -red $MIN > tmp.yasmet.$i.$MIN; 
-	cat tmp.yasmet.$i.$MIN | $YASMET > tmp.lambdas.$i
-	cat tmp.lambdas.$i | sed "s/^/$i /g" >> data-$SL-$TL/all-lambdas;
-done
+	cat 'tmp.yasmet.$i' | $YASMET -red $MIN > 'tmp.yasmet.$i.$MIN'; 
+	cat 'tmp.yasmet.$i.$MIN' | $YASMET > 'tmp.lambdas.$i'
+	cat 'tmp.lambdas.$i' | sed "s/ /\t/g" | sed "s/^/$i\t/g" >> data-$SL-$TL/all-lambdas;
+	# IFS='\n';
+done < tmp.sl
+# IFS=$OLDIFS
 
 rm tmp.*
 
