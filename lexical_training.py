ccfe020961832bd42050cbefefee68f810688096
vivekvardhanadepu <vivekvicky839@gmail.com>
2021-06-04 09:43:04 +0530

    moving repo to apertium-lexical-training

--- lexical_training.py
+++
@@ -1,9 +0,0 @@
-# lexical training script
-from check_config import check_config
-
-def main():
-    config = check_config()
-    print("parsing complete")
-    
-if __name__ == '__main__':
-    main()
